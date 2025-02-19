f541451797e11914b12645513a966a17be0f052c
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-19 21:04:01 +0000

    added int literals

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number = 1;
+            Console.WriteLine(number);
+
+            int binary_int = 0b1000_0001_0001_0001;
+            Console.WriteLine(binary_int);
+
+            int hexadecimal_int = 0xFF;
+            Console.WriteLine(hexadecimal_int);
+
+            uint second_number = 123456789;
+            Console.WriteLine(second_number);
+
+
+        }
+    }
+}
