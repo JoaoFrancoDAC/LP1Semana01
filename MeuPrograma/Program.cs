58dde3884a40878a918237331a00bff648789473
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-19 20:41:21 +0000

    Exercose number 2. Added comments with ///

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// triple "/" before class 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// triple "/" before main
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
