2bef55d8123b216a1f54ec74ab08b8bae9cdc4ab
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-20 20:31:42 +0000

    added new project with some strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; 
+            Console.WriteLine("Hello LP1!");
+
+             string s = "Um tab\t e um copyright\u03A9";
+             Console.WriteLine(s);
+
+             string test = "using unicode characters: \u00A9";
+             Console.WriteLine(test);
+
+             string umaLetraPorLinha = "a\n b\nc\nd\ne\nf\ng\nh\ni\nj\nk\nl\nm\nn\no\np\nq\nr\ns\nt\nu\nv";
+             Console.WriteLine(umaLetraPorLinha);
+
+
+        }
+    }
+}
