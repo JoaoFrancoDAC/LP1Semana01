7b61eea9592891eb2ce05c5f4921cc2400682b00
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-20 21:09:04 +0000

    added the sum of the 2 inputs. Already converted

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -15,6 +15,12 @@ namespace AskForInput
             string real_a_ser_convertido = Console.ReadLine();
             Console.WriteLine($"real ainda em formato de string: {real_a_ser_convertido}");
 
+            float real_convertido = float.Parse(real_a_ser_convertido);
+            Console.WriteLine($"real convertido para float: {real_convertido}");
+
+            Console.WriteLine($"Soma dos dois numeros: {real_convertido + numero_inteiro}");
+
+
 
 
 
