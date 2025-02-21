3e05755f4c56c6e94eb0e0cc50a94cbc5d0ad7a2
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-02-21 17:39:53 +0000

    Exercício de avaliação da aula de sexta-feira

---
+++ Smiley/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Insere código:");
+            string numero_hexadecimal = Console.ReadLine();
+
+            char caráter_unicode = (char)int.Parse(numero_hexadecimal, NumberStyles.HexNumber);
+            Console.WriteLine($"O caractere correspondente é: {caráter_unicode}");
+
+        }
+    }
+}
